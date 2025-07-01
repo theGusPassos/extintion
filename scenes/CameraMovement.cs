1ed54521ea3a0f24f9f4fdc4f9b93b0522afd3ba
theguspassos <theguspassos@gmail.com>
2025-07-01 19:11:24 -0300

    add camera movement script

---
+++ scenes/CameraMovement.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class CameraMovement : Camera3D
+{
+	[Export] float movementSpeed;
+	[Export] Vector3 mainMenuPosition;
+	[Export] Vector3 positionInGame;
+
+	bool isMovingToInGamePos;
+	float movementT;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Position = mainMenuPosition;
+		movementT = 0;
+	}
+
+	public override void _Process(double delta)
+	{
+		if (isMovingToInGamePos)
+		{
+			movementT += (float)delta * movementSpeed;
+			Position = mainMenuPosition.Lerp(positionInGame, movementT);
+		}
+	}
+}
